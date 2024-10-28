c0abb6b31d5db848ca8166ca8ebf5c87c13d2f8a
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-28 14:57:06 +0100

    complete assigments

--- main.js
+++ main.js
@@ -9,3 +9,22 @@
 // se il valore dell'argomento é admin ed il valore di PIN definito é 666 allora stampa in console Welcome Admin
 // altrimenti stampa Access restricted
 
+// creo un semplice console log
+console.log('Hello node js');
+
+// uso il modulo process per precuperare la variabile d'ambiente
+const pin = process.env.PIN;
+// stampo in console la variabile PIN
+console.log(`PIN: ${pin}`);
+
+// recupero l'argomento riga di comando alla posizione index 2 
+const argomento = process.argv[2];
+// stampo in console la variabile argomento
+console.log(`Argomento passato: ${argomento}`);
+
+// Verifica PIN e argomento
+if (argomento === "admin" && pin === "666") {
+    console.log("Welcome Admin");
+} else {
+    console.log("Access restricted");
+}
